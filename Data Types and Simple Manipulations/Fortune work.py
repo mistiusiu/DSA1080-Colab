6752ef8d81a0f43a5a5464134de1073310ade28d
mistiusiu <158315455+mistiusiu@users.noreply.github.com>
2024-02-01 13:10:30 +0300

    Small small changes

--- Data Types and Simple Manipulations/Fortune work.py
+++ Data Types and Simple Manipulations/Fortune work.py
@@ -6,19 +6,19 @@ fruits.pop()
 print(fruits)
 
 #tuple exploration
-tuple = ("Purple", "Red", "Pink", "Black",)
-y = list (tuple)
+my_tuple = ("Purple", "Red", "Pink", "Black",)
+y = list(my_tuple)
 y.append("Silver")
-tuple = tuple(y)
+my_tuple = tuple(y)
 
 fruits = ["Orange", "Apple", "Kiwi", "Avacado"]
-fruits =(fruits[0]+","+ fruits[-1])
+fruits =[fruits[0], fruits[-1]]
 print(fruits)
 fruits = (fruits[1:5:2])
 print(fruits)
 
-combine the fruits list and the colours tuple into a new list named combined
+# combine the fruits list and the colours tuple into a new list named combined
 colours = ('Red', 'Blue', 'Pink', 'Purple')
 fruits = ["Orange", "Apple", "Kiwi", "Avacado"]
-combined = fruits + colours
+combined = fruits + list(colours)
 print(combined)
