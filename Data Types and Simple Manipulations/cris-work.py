957eeddd2eaa1b9459416dec4a3abf94c8b484d3
helacris007 <crispinndungu@gmail.com>
2024-02-01 13:01:28 +0300

    Create cris-work.py

---
+++ Data Types and Simple Manipulations/cris-work.py
@@ -0,0 +1,48 @@
+#list
+fruits=["apple","mango","avocado","kiwi","pawpaw"]
+
+#adding grape to the list
+fruits.append("grape")
+print(fruits)
+
+#replacing "mango" with "banana"
+fruits[1]= "banana"
+print(fruits)
+#remove grape from the list
+fruits.remove("grape")
+print(fruits)
+
+
+colours = ("red","blue","purple","pink")
+
+colours.insert(1,"green")
+print(colours)
+
+#QUESTION 2
+thistuple = ("red","black","pink","purple")
+y = list(thistuple)
+y.append("siver")
+thistuple = tuple(y)
+print(thistuple)
+
+#QUESTION 3
+fruits = ["apple","mango","avocado","kiwi","pawpaw"]
+print(fruits[0],fruits[-1])
+
+fruits = ["apple","mango","avocado","kiwi","pawpaw"]
+s = fruits[::2]
+print(s)
+
+#QUESTION 4
+fruits = ["apple", "banana", "orange"]
+colors = ("red", "green", "blue")
+
+# Combine the 'fruits' list and the 'colors' tuple into a new list named 'combined'
+combined = fruits + list(colors)
+
+
+
+
+
+
+
